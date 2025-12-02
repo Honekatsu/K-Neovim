ca6e6984ae5a413927dbb37627c5a65afb47af2d
ReyoKatsu <38076269+Honekatsu@users.noreply.github.com>
2025-12-02 10:35:23 +0900

    feat(keymap): Add key mappings for window navigation

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,5 +1,16 @@
+vim.g.mapleader = " "
 vim.keymap.set("i", "{", "{}<Left>")
 vim.keymap.set("i", "(", "()<Left>")
+
+-- Window navigation
+vim.keymap.set('n', '<S-h>', '<C-w>h', { desc = 'Go to left window' })  -- 左のウィンドウへ移動
+vim.keymap.set('n', '<S-j>', '<C-w>j', { desc = 'Go to lower window' }) -- 下のウィンドウへ移動
+vim.keymap.set('n', '<S-k>', '<C-w>k', { desc = 'Go to upper window' }) -- 上のウィンドウへ移動
+vim.keymap.set('n', '<S-l>', '<C-w>l', { desc = 'Go to right window' }) -- 右のウィンドウへ移動
+-- Buffer navigation
+vim.keymap.set('n', '<leader>h', ':bprev<CR>', { desc = 'Previous buffer' })
+vim.keymap.set('n', '<leader>l', ':bnext<CR>', { desc = 'Next buffer' })
+
 -- Neo-tree
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
 -- comment out
