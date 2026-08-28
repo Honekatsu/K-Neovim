55fe7704db8ab283944d5e5f89cdafa1a6263e43
ReyoKatsu <38076269+Honekatsu@users.noreply.github.com>
2026-08-28 10:04:59 +0900

    feat: add BlueScript Syntax Highlight

--- lua/config/filetype.lua
+++ lua/config/filetype.lua
@@ -1,8 +1,10 @@
 vim.filetype.add({
   extension = {
     mdx = "mdx",
-    spin = "promela"
+    spin = "promela",
+    bs = "bs"
   },
 })
 vim.treesitter.language.register("markdown", { "mdx" })
 vim.treesitter.language.register("c", { "promela" })
+vim.treesitter.language.register("typescript", { "bs" })
